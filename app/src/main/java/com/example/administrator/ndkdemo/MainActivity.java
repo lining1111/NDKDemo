ab8bb6e123834735a5b7a0fdfaa7b43c0d226ede
李宁 <770677655@qq.com>
2017-07-10 14:14:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/ndkdemo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.administrator.ndkdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        callBackJavafromC();
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    public native void callBackJavafromC();
+
+    public native void hello();
+
+    public void logout(){
+        System.out.println("hahahahaha");
+    }
+}
---
+++ app/src/main/jni/native-lib.c
@@ -0,0 +1,34 @@
+#include <jni.h>
+
+//extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_administrator_ndkdemo_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject instance) {
+//    std::string hello = "Hello from C++";
+//    return env->NewStringUTF(hello.c_str());
+
+    char *c ="hello from c";
+    return (*env)->NewStringUTF(env,c);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_administrator_ndkdemo_MainActivity_callBackJavafromC(
+        JNIEnv *env,
+        jobject instance){
+    //1.通过反射找到类
+    jclass clazz;
+    clazz = (*env)->FindClass(env,"com/example/administrator/ndkdemo/MainActivity");
+
+    //2.找到方法ID
+    jmethodID methodId = (*env)->GetMethodID(env,clazz,"logout","()V");
+    //3.调用方法，obj就是调用的类实例，所以不用再次创建
+    (*env)->CallVoidMethod(env,instance,methodId);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_administrator_ndkdemo_MainActivity_hello(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+}
